26429b3b1dfb92a268ebcfc2ea60e2888b02d3b6
1Mashilo <christophermashilo@gmail.com>
2023-12-31 16:04:02 +0100

    "Add product list view and template

--- multivendor/vendor/urls.py
+++ multivendor/vendor/urls.py
@@ -1,7 +1,9 @@
 # vendor/urls.py
 from django.urls import path
 from .views import index
+from .views import product_list
 
 urlpatterns = [
     path('', index, name='index'),
+    path('products/', product_list, name='product_list'),
 ]
--- multivendor/vendor/views.py
+++ multivendor/vendor/views.py
@@ -1,5 +1,11 @@
 # vendor/views.py
 from django.shortcuts import render
+from .models import Product
 
 def index(request):
     return render(request, 'vendor/index.html')
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'vendor/product_list.html', {'products': products})
+
