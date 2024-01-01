67f1408f228ae6440152efa41470fd22198d3d41
1Mashilo <christophermashilo@gmail.com>
2024-01-01 12:03:50 +0100

    Added update and delete functionalities for products  - Created new views to edit and delete a product with confirmation  - Updated URLs to include the edit_product and delete_product patterns  - Added new HTML templates for product editing and deletion confirmation  - Update existing templates for consistency, styling, and form handling  - Modify forms and views to support edit and delete operations  - Included relevant changes to handle product updates and deletions  - Minor fixes and improvements

--- multivendor/vendor/urls.py
+++ multivendor/vendor/urls.py
@@ -1,11 +1,13 @@
 # urls.py
 from django.urls import path
-from .views import index, product_list, product_detail,add_product
+from .views import index, product_list, product_detail,add_product,edit_product
+
 
 urlpatterns = [
     path('', index, name='index'),
-    path('products/', product_list, name='product-list'),
+    path('products/', product_list, name='product_list'),
     path('products/<int:pk>/', product_detail, name='product-detail'),
     path('add_product/', add_product, name='add_product'),
-
+    path('edit_product/<int:pk>/', edit_product, name='edit_product'),
 ]
+
--- multivendor/vendor/views.py
+++ multivendor/vendor/views.py
@@ -1,5 +1,5 @@
 # views.py
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product
 from .forms import ProductForm
 
@@ -19,8 +19,22 @@ def add_product(request):
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('product_list')  # Assuming you have a URL pattern named 'product_list'
+            return redirect('product_list')
     else:
         form = ProductForm()
 
     return render(request, 'vendor/add_product.html', {'form': form})
+
+
+def edit_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm(instance=product)
+
+    return render(request, 'vendor/edit_product.html', {'form': form, 'product': product})
