b47152022f0645121ab7dbe8d0f66c65b21b698e
1Mashilo <christophermashilo@gmail.com>
2024-01-02 11:57:24 +0100

    i have updated the Product model to associated the product with the seller

--- multivendor/vendor/models.py
+++ multivendor/vendor/models.py
@@ -1,6 +1,13 @@
-# vendor/models.py
-
 from django.db import models
+from django.contrib.auth.models import User
+
+class Seller(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # Add any additional fields related to the seller profile if needed
+    # For example: seller_name, seller_location, etc.
+
+    def __str__(self):
+        return self.user.username
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
@@ -9,10 +16,11 @@ class Product(models.Model):
     product_file = models.FileField(upload_to='product_files/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)  # Add this line to associate with Seller
+
     def __str__(self):
         return self.name
-    
+
     
     def get_absolute_url(self):
         return reverse('product-detail', args=[str(self.id)])
