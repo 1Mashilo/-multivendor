a01ba991011e562c6a0c250671593f8dac823a26
1Mashilo <christophermashilo@gmail.com>
2024-01-02 13:50:22 +0100

    updated models to include purchashes

--- multivendor/vendor/models.py
+++ multivendor/vendor/models.py
@@ -16,3 +16,11 @@ class Product(models.Model):
     
     def get_absolute_url(self):
         return reverse('product-detail', args=[str(self.id)])
+
+class Purchase(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    purchase_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} purchased {self.product.name}"
